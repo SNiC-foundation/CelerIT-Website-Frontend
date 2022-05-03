5c52bbb4ea9171323db644eee43dcb6dfc8fe730
Leon Vreling <l.m.vreling@student.tue.nl>
2022-05-03 12:12:29 +0200

    gitflow-feature-stash: login

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import App from './views/App';
+import Login from './views/auth/Login';
 
 function Router() {
   return (
     <Routes>
       <Route path="" element={<App />} />
+      <Route path="login" element={<Login />} />
     </Routes>
   );
 }
